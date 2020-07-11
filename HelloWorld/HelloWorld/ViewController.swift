8c4425a8b4b631f10b4cdb2731f9f7cdae5382cc
Gabrielle Viray <gabrielleviray@Gabrielles-MacBook-Pro.local>
2020-07-10 18:09:16 -0700

    Practice Layout Buttons

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Gabrielle Viray on 7/10/20.
+//  Copyright © 2020 Gabrielle Viray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
